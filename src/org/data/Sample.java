40c6f39104516985a0e2f2120f34dc43a1f989b2
AravindS2096 <su.aravindsuresh@gmail.com>
2022-07-02 15:54:59 +0530

    Code is done

--- src/org/data/Sample.java
+++ src/org/data/Sample.java
@@ -2,6 +2,11 @@ package org.data;
 
 public class Sample {
 	
+	public void m3()
+	{
+		System.out.println("Bala code");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Work is done");
